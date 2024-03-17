1d5c83cfa12fe5b2d3527c9dd744ff2c52e2c49e
Dmitry Rogozhin <kuvbur@gmail.com>
2024-03-17 20:30:07 +0300

    update

---
+++ Tool/!archibibl.bat
@@ -0,0 +1,34 @@
+cd /d %~dp0
+cd ..
+set "converter=C:\Program Files\GRAPHISOFT\ARCHICAD 25\LP_XMLConverter.exe"
+set "tool_dir=%cd%\Tool"
+set "gdl_dir=%cd%\gdl"
+set "hsf_dir=%cd%\hsf"
+set "lcf_dir=%cd%\lcf"
+
+set "gdl_1_dir=%gdl_dir%\_ArchiBibl"
+
+set "hsf_1_dir=%hsf_dir%\_ArchiBibl"
+set "lcf_1=%lcf_dir%\archbib24.lcf"
+
+
+RD /S /Q  "%hsf_1_dir%"
+
+MD "%hsf_1_dir%"
+
+cd "%hsf_dir%"
+"%converter%"   l2hsf -l CYR "%gdl_1_dir%" "%hsf_1_dir%" >"%tool_dir%\_ArchiBibl_hsf_log.txt"
+
+cd "%lcf_dir%"
+"%converter%" createcontainer "%lcf_1%" -compress 9 "%gdl_1_dir%">"%tool_dir%\_ArchiBibl_log.txt"
+
+
+TIMEOUT /T 1
+
+cd ..
+cd ..
+
+ROBOCOPY "%cd%\gdl_macro\lcf" "%lcf_dir%" /E>"%tool_dir%\_copy_log.txt"
+ROBOCOPY "%cd%\gdl_macro\lcf" "%cd%\LCF24" /E>"%tool_dir%\_copy_log.txt"
+
+ROBOCOPY "%lcf_dir%" "%cd%\LCF24" archbib24.lcf /E>"%tool_dir%\_copy_log.txt"

0abe15692a75c87e9c67bbc10ab1131b8e1b7c78
Dmitry Rogozhin <kuvbur@gmail.com>
2025-03-11 15:55:08 +0300

    Добавил помещение с отделкой

--- Tool/!mep.bat
+++ Tool/!mep.bat
@@ -6,33 +6,27 @@ set "gdl_dir=%cd%\gdl"
 set "hsf_dir=%cd%\hsf"
 set "lcf_dir=%cd%\lcf"
 
-set "gdl_1_dir=%gdl_dir%\_ArchiBibl"
 set "gdl_3_dir=%gdl_dir%\_MEP"
 
-set "hsf_1_dir=%hsf_dir%\_ArchiBibl"
 set "hsf_3_dir=%hsf_dir%\_MEP"
 
-set "lcf_1=%lcf_dir%\archbib24.lcf"
 set "lcf_3=%lcf_dir%\mep24.lcf"
 
-RD /S /Q  "%hsf_1_dir%"
 RD /S /Q  "%hsf_3_dir%"
-MD "%hsf_1_dir%"
+
 MD "%hsf_3_dir%"
 
 cd "%hsf_dir%"
-"%converter%"   l2hsf -l CYR "%gdl_1_dir%" "%hsf_1_dir%" >"%tool_dir%\log\_ArchiBibl_hsf_log.txt"
+
 "%converter%"   l2hsf -l CYR "%gdl_3_dir%" "%hsf_3_dir%" >"%tool_dir%\log\_MEP_hsf_log.txt"
 
 cd "%lcf_dir%"
-"%converter%" createcontainer "%lcf_1%" -compress 9 "%gdl_1_dir%">"%tool_dir%\log\_ArchiBibl_log.txt"
+
 "%converter%" createcontainer "%lcf_3%" -compress 9 "%gdl_3_dir%">"%tool_dir%\log\_MEP_log.txt"
 
 python "%tool_dir%\hsf2text.py">"%tool_dir%\log\_copy_log.txt"
 
 TIMEOUT /T 1
-
-python "%tool_dir%\build_lcf.py">"%tool_dir%\log\_copy_log.txt"
 
 cd ..
 cd ..
@@ -40,5 +34,4 @@ cd ..
 ROBOCOPY "%cd%\gdl_macro\lcf" "%lcf_dir%" /E>"%tool_dir%\log\_copy_log.txt"
 ROBOCOPY "%cd%\gdl_macro\lcf" "%cd%\LCF25" /E>"%tool_dir%\log\_copy_log.txt"
 
-ROBOCOPY "%lcf_dir%" "%cd%\LCF25" archbib24.lcf /E>"%tool_dir%\log\_copy_log.txt"
 ROBOCOPY "%lcf_dir%" "%cd%\LCF25" mep24.lcf /E>"%tool_dir%\log\_copy_log.txt"
